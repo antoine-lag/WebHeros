3f3a6b5d821067810c15be820182e0566ccb8eca
antoine-lag <lagrange.antoine@gmail.com>
2021-05-20 09:16:47 +0200

    situation situationclasse

--- src/pack/data/Situation.java
+++ src/pack/data/Situation.java
@@ -1,5 +1,58 @@
 package pack.data;
 
-public class Situation {
+import java.util.*;
+import javax.persistence.*;
 
+@Entity
+public class Situation {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int id;
+	
+	String texte;
+	
+	@OneToMany
+	Collection<Choix> choix;
+	
+	@OneToOne
+	Moderation moderation;
+	
+	public Situation() {}
+
+	/**
+	 * @param texte
+	 * @param choix
+	 * @param moderation
+	 */
+	public Situation(String texte, Collection<Choix> choix, Moderation moderation) {
+		super();
+		this.texte = texte;
+		this.choix = choix;
+		this.moderation = moderation;
+	}
+
+	public String getTexte() {
+		return texte;
+	}
+
+	public void setTexte(String texte) {
+		this.texte = texte;
+	}
+
+	public Collection<Choix> getChoix() {
+		return choix;
+	}
+
+	public void setChoix(Collection<Choix> choix) {
+		this.choix = choix;
+	}
+
+	public Moderation getModeration() {
+		return moderation;
+	}
+
+	public void setModeration(Moderation moderation) {
+		this.moderation = moderation;
+	};
 }
--- src/pack/data/SituationClasse.java
+++ src/pack/data/SituationClasse.java
@@ -1,5 +1,19 @@
 package pack.data;
 
+import javax.persistence.*;
+
+@Entity
 public class SituationClasse {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	int id;
+	
+	int ordre;
+	
+	@ManyToOne
+	Situation situation;
+	
+	public SituationClasse() {};
 
 }
