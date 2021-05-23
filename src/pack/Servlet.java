55aacf791626066732e70885eb2a387448199869
Coupry Benjamin <bcoupry@truite.enseeiht.fr>
2021-05-23 11:52:10 +0200

    html ajout de situation

--- src/pack/Servlet.java
+++ src/pack/Servlet.java
@@ -1,6 +1,10 @@
 package pack;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -8,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import pack.data.Jeu;
 import pack.data.Utilisateur;
@@ -48,8 +53,18 @@ public class Servlet extends HttpServlet {
 			String stringSetup = "Setup done !\n joueur="+facade.getIDJoueur("Bob", id_jeu);
 			response.getWriter().println("<html><body>"+stringSetup+"</body></html>");
 		}*/
-		if (request.getParameter("")) {
+		if (request.getParameter("mode").equals("ajoutSituation")) {
+			HttpSession session = request.getSession(false);
+			int idJoueur = (int)(session.getAttribute("idJoueur"));
+			int idAventure = (int)(session.getAttribute("idAventure"));
+			int idChoixSoure = (int)(session.getAttribute("idChoixSource"));
+			String texteSituation = request.getParameter("texteSituation");
+		
+			List<String> textesOptions = Arrays.asList(request.getParameter("choixSuite"));
 			
+			facade.affilierSituationFille(idChoixSoure, texteSituation, textesOptions, idJoueur, idAventure);
+			String stringSetup = "Situation ajoutee";
+			response.getWriter().println("<html><body>"+stringSetup+"</body></html>");
 		}
 	}
 
@@ -58,6 +73,6 @@ public class Servlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 }
